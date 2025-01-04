36ed17a78edb6d44ddf40c3e1075ed1a63845c46
Avinash Chaurasiya <avinashchaurasiya902@gmail.com>
2025-01-04 11:54:32 +0000

    update

--- MyAlumni/MyAlumni/settings.py
+++ MyAlumni/MyAlumni/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,9 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-        BASE_DIR / "static",  # or 'myapp/static'
-        ]
+STATICFILES_DIRS=[
+	os.path.join(BASE_DIR,'static')
+	]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
