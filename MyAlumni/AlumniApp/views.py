56a1c258752b03f31638c56ff1d56c127ddcf0ab
webdevavi96 <avinaschaurasiya902@gmail.com>
2025-01-17 13:15:09 +0530

    update

--- MyAlumni/AlumniApp/views.py
+++ MyAlumni/AlumniApp/views.py
@@ -7,3 +7,11 @@ def home(request):
     
 def about(request):
   return render(request, "master.html")
+  
+def events(request):
+    events = [
+        {'id': 1, 'details': 'Welcome to Alumni Meet 2025', 'status': 'Upcoming', 'date_time': 'Monday 20 Jan 2PM'},
+        {'id': 2, 'details': 'Technical Workshop by Experts', 'status': 'Ongoing', 'date_time': 'Tuesday 21 Jan 10AM'},
+        {'id': 3, 'details': 'Cultural Program & Networking', 'status': 'Ended', 'date_time': 'Sunday 18 Jan 5PM'},
+    ]
+    return render(request, 'static/pages/events.html', {'events': events})
--- MyAlumni/MyAlumni/urls.py
+++ MyAlumni/MyAlumni/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home),
     path("about/", views.about),
+    path("events/", views.events),
 ]
