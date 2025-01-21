e26dc91235f3d277d528d882d280ebd38f5c68d0
webdevavi96 <avinaschaurasiya902@gmail.com>
2025-01-21 16:55:55 +0530

    update

--- MyAlumni/static/scripts/min.js
+++ MyAlumni/static/scripts/min.js
@@ -1,3 +1,13 @@
-setTimeout(() =>{
-  alert("hello!")
-},2000);
+const navBtn = document.querySelector('.nav-btn');
+
+function navExpand() {
+  const navItems = document.querySelector('.nav-menu-items');
+  if (navItems) {
+    navItems.classList.toggle('active'); // Toggle class
+  }
+  console.log('Clicked');
+}
+
+if (navBtn) {
+  navBtn.addEventListener('click', navExpand);
+}
