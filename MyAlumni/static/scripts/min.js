c33c7c1ccc460b85f47c6a710c2317734f540428
webdevavi96 <avinaschaurasiya902@gmail.com>
2025-02-03 14:18:45 +0530

    update

--- MyAlumni/static/scripts/min.js
+++ MyAlumni/static/scripts/min.js
@@ -1,26 +1,10 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const navLinks = document.querySelectorAll('.nav-link');
+document.addEventListener("DOMContentLoaded", function () {
+    let currentPath = window.location.pathname;
+    let navLinks = document.querySelectorAll(".nav-link");
 
-//   navLinks.forEach((link) => {
-//     link.addEventListener('click', (event) => {
-//       event.preventDefault();
-//       navLinks.forEach(nav => nav.classList.remove('active'));
-//       link.classList.add('active');
-
-//       const requestUrl = link.getAttribute('href');
-//       fetch(requestUrl)
-//         .then(response => response.text())
-//         .then(data => {
-//           document.getElementById('content').innerHTML = data;
-//         })
-//         .catch(error => console.error('Error loading content:', error));
-//     });
-//   });
-
-//   const activeLink = window.location.pathname;
-//   navLinks.forEach((link) => {
-//     if (link.getAttribute('href') === activeLink) {
-//       link.classList.add('active');
-//     }
-//   });
-// });
+    navLinks.forEach(link => {
+      if (link.getAttribute("href") === currentPath) {
+        link.classList.add("active");
+      }
+    });
+  });
