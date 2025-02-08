6d8616b7610fbe5df78219b2126e5fa7e527f0b7
webdevavi96 <avinaschaurasiya902@gmail.com>
2025-02-08 14:51:47 +0530

    update urls.py

--- MyAlumni/MyAlumni/urls.py
+++ MyAlumni/MyAlumni/urls.py
@@ -21,7 +21,7 @@ from AlumniApp import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home name = "home"),
+    path("", views.home, name = "home"),
     path("about/", views.about, name = about),
     path("events/", views.events, name = "events"),
     path("blogs/", views.blogs, name = "blogs"),
