5f6b24c148b912033a5fcae30bad76c15cac46ba
webdevavi96 <avinaschaurasiya902@gmail.com>
2025-03-18 13:39:06 +0530

    update

--- MyAlumni/MyAlumni/settings.py
+++ MyAlumni/MyAlumni/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "AlumniApp",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://super-happiness-7vprqj4vw6pg2p474-8000.app.github.dev",  # Allow frontend access
 ]
 
 ROOT_URLCONF = "MyAlumni.urls"
--- MyAlumni/static/scripts/api.js
+++ MyAlumni/static/scripts/api.js
@@ -1,5 +1,7 @@
 export async function test(){
-  const response = await fetch("http://127.0.0.1:8000/api/data")
+  const response = await fetch("https://super-happiness-7vprqj4vw6pg2p474-8000.app.github.dev/api/data/")
   const data = await response.json()
+  .catch(error)
+  console.log(error)
  return data
 }
