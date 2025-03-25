2ba3880b84a7ae9495797713a58c59ecc2d870ae
webdevavi96 <avinaschaurasiya902@gmail.com>
2025-03-25 14:34:46 +0530

    update urls.py

--- MyAlumni/MyAlumni/urls.py
+++ MyAlumni/MyAlumni/urls.py
@@ -21,20 +21,18 @@ from AlumniApp import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home, name = "home"),
-    path("about/", views.about, name = "about"),
-    path("events/", views.events, name = "events"),
-    path("blogs/", views.blogs, name = "blogs"),
-    path("register/", views.register, name = "register"),
-    path("gallery/", views.gallery, name = "gallery"),
-    path("contact-us", views.contactUs, name = "contact-us"),
-    path("profile/", views.userProfile, name = "profile"),
-    path("register/", views.register, name = "register"),
-    path("accounts/login/", views.login, name = "login"),
-    path("api/data/", views.get_data, name = "get_data"),
-    path("api/verify_user/", views.verify_user, name = "verify_user"),
-    path("api/register_user/", views.os.path.join(BASE_DIR,"static"), name = "register_user"),
-] + static(settings.MEDIA_URL, document_root.settings.MEDIA_ROOT)
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("events/", views.events, name="events"),
+    path("blogs/", views.blogs, name="blogs"),
+    path("register/", views.register, name="register"),  # Keep only one
+    path("gallery/", views.gallery, name="gallery"),
+    path("contact-us/", views.contactUs, name="contact-us"),
+    path("profile/", views.userProfile, name="profile"),
+    path("accounts/login/", views.login, name="login"),
+    path("api/data/", views.get_data, name="get_data"),
+    path("api/verify_user/", views.verify_user, name="verify_user"),
+    path("api/register_user/", views.register_user, name="register_user"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
